29e98e3a2065222dffe0137e5c0d9ddd0ba4ce37
Jonathan Chichioco <jonathan.n.chichioco@gmail.com>
2018-10-23 23:57:29 -0700

    use index.html

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/search/:page', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
 app.listen(port, () => {
