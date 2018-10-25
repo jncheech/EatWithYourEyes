5e650811a4d0a400f333a8ebaaa9e8311be60897
Jonathan Chichioco <jonathan.n.chichioco@gmail.com>
2018-10-25 08:52:44 -0700

    Fix for empty set after last page

--- server/yelp-search.js
+++ server/yelp-search.js
@@ -15,18 +15,18 @@ var YelpSearch = {
         let storedUrls = memcache.getCache(key_b) || {};
         
         if (parseInt(page) > 0 && storedBusinesses[location].length > 5) {
-            console.log('additional pages');
+            console.log('Additional pages');
             this.getAdditionalPages(location, page, success, fail);
         } else if (typeof storedBusinesses[location] === 'undefined' || typeof storedUrls[location] === 'undefined'){
-            console.log('wide search');
+            console.log('Wide search');
             this.getWideBusinesses(location, success, fail);
         } else {
-            console.log('businesses stored already for location', location);            
+            console.log('Businesses stored already for location', location);            
             success(this.constructResolvedPromise(location));
         }
     },
     getAdditionalPages: function(location, page, success, fail) {
-        console.log('next page?');
+        console.log('next page?', page);
         var urls = [];
         let storedBusinesses = {};
         let storedUrls = {};
@@ -38,7 +38,7 @@ var YelpSearch = {
                 let key = memcache.keyify('stored_urls', location);
                 storedUrls = memcache.getCache(key);
                 if (!storedUrls[storedBusinesses[location][i]]) {
-                    console.log(`store search for ${storedBusinesses[location][i]}`)
+                    console.log(`Store search for ${storedBusinesses[location][i]}`)
                     urls.push(`https://api.yelp.com/v3/businesses/${storedBusinesses[location][i]}`) ;
                 } else {
                     console.log(`${storedBusinesses[location][i]} is stored`);
@@ -47,26 +47,31 @@ var YelpSearch = {
         } 
 
         let promiseArray = urls.map( url => axios.get(url, { headers: config } ));
-            axios.all(promiseArray)
-            .then( results => {
-                results.map( r => {
-                    storedUrls[location][encodeURI(r.data.alias)] = {};
-                    storedUrls[location][encodeURI(r.data.alias)]['name'] = r.data.name;
-                    storedUrls[location][encodeURI(r.data.alias)]['rating'] = r.data.rating;
-                    storedUrls[location][encodeURI(r.data.alias)]['photos'] = [];
-                    r.data.photos.forEach( (photo) => {
-                        storedUrls[location][encodeURI(r.data.alias)]['photos'].push(photo);
-                    })
-                });
-                
-                let key = memcache.keyify('stored_urls', location);
-                memcache.setCache(key, storedUrls);
-                success(this.constructResolvedPromise(location));
-            })
-            .catch( err => {
-                console.log(err);
-                fail(err);
+        if (promiseArray.length == 0) {
+            success(this.constructResolvedPromise(location));
+            return;
+        }
+        
+        axios.all(promiseArray)
+        .then( results => {
+            results.map( r => {
+                storedUrls[location][encodeURI(r.data.alias)] = {};
+                storedUrls[location][encodeURI(r.data.alias)]['name'] = r.data.name;
+                storedUrls[location][encodeURI(r.data.alias)]['rating'] = r.data.rating;
+                storedUrls[location][encodeURI(r.data.alias)]['photos'] = [];
+                r.data.photos.forEach( (photo) => {
+                    storedUrls[location][encodeURI(r.data.alias)]['photos'].push(photo);
+                })
             });
+            
+            let key = memcache.keyify('stored_urls', location);
+            memcache.setCache(key, storedUrls);
+            success(this.constructResolvedPromise(location));
+        })
+        .catch( err => {
+            console.log(err);
+            fail(err);
+        });
     },
     getWideBusinesses: function(location, success, fail) {
         let storedBusinesses = {};
